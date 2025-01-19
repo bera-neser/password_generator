99b990421ea4264a96437537b71045fe1ef1e99a
Elif Ezgi Yıldız <71594152+eezgiy@users.noreply.github.com>
2025-01-19 22:31:52 +0300

    Update Password_Generator_Project.py

--- Password_Generator_Project.py
+++ Password_Generator_Project.py
@@ -1,14 +1,10 @@
 #Password Generator Project
 import random
+import string
 
-letters = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
-    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-]
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+letters = string.ascii_letters
+numbers = string.digits
+symbols = string.punctuation
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
